b466dfa69a479ff0c648d5aa21c97c16d7683121
noelAAlmeida <noelnik20@hotmail.com>
2021-04-22 19:59:27 -0300

    criando aside

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,88 +1,56 @@
-/*
+///import{funcoes} from "./funcoes";
+/// MENU DE NAVEGAÇAO DO HEADER CRIADO DINAMICAMENTE.
 
-function sizeOfThings(){
-  var windowWidth = window.innerWidth;
-  var windowHeight = window.innerHeight;
+//criando menu (ul, li, a)
+const elMenu = document.getElementById('navbar');
+const elMenuH1 = document.querySelector('.header-nav');
+const menuUl = document.createElement('ul');
+const aAA = document.querySelector('nav>ul');
+const bBB = document.querySelector('.header-nav>ul');
+const arrayClassesMenu = ["fa-twitter","fa-facebook-f","fa-youtube","fa-instagram-square","fa-github","fa-linkedin-in"];
   
-  var screenWidth = screen.width;
-  var screenHeight = screen.height;
-  
-  document.querySelector('.window-size').innerHTML = windowWidth + 'x' + windowHeight;
-  document.querySelector('.screen-size').innerHTML = screenWidth + 'x' + screenHeight;
 
-};
+function addLinks(){ ///LINKS -----------------------
+  for(let i=0;i<arrayClassesMenu.length;i++){
+    const menuLi = document.createElement('li'),
+          menuLink = document.createElement('a');
+          const addMenuLi  = menuUl.appendChild(menuLi);
+          addMenuLi.classList.add("nav-item");
 
-sizeOfThings();
-
-window.addEventListener('resize', function(){
-	sizeOfThings();
-});
-
-
-
-
-x = window.screen.width;
-y = window.screen.height;
-
-if(x <800){
-//Ação aqui
+    const addMenuLink = menuLi.appendChild(menuLink);
+          addMenuLink.setAttribute('href','#');
+          addMenuLink.classList.add("nav-link","fab",arrayClassesMenu[i]);
+}
 }
 
 
-*/
-///REMOVENDO O ELEMENTO MENU ATRAVES DO ELEMENTO PAI.
-/*
-var pegando = document.querySelector('.navbar');
-console.log(pegando);
-var remov = document.querySelector('.navbar-nav');
-var remopvido = pegando.removeChild(remov);
-*/
-
-///REMOVENDO O ELEMENTO MENU SEM CONHECER O PAI
-/*
-var no = document.querySelector(".navbar-nav");
-if (no.parentNode) {
-  no.parentNode.removeChild(no);
-}
-console.log(no);
-*/
-
-//// BREAKPOINTS BOOTSTRAP
-/*
-xs: 0,
-  sm: 576px,
-  md: 768px,
-  lg: 992px,
-  xl: 1200px
-*/
-/*var largJanela = window.screen.width;
-console.log(largJanela);
-
-document.write(screen.width+'x'+screen.height);
-   
-   //resolução 'real' navegador
-   var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-   console.log(width);*/
-   
-   ///----------------------
-   /*const consultaMedia = window.matchMedia('min-width:768');
-   if (consultaMedia.matches) {
-    const no = document.querySelector(".navbar-nav");
-    if (no.parentNode) {
-    no.parentNode.removeChild(no);
-}
-  } else {
-    /* a viewport menos que 400 pixels de largura */
-  /*}*/
- /* const consultaMedia = window.matchMedia('(min-width:768)');
-  function menuMediaQuery(consultaMedia){
-      if(consultaMedia.matches){
-        const removeMenu = document.querySelector(".navbar-nav");
-        if (removeMenu.parentNode) {
-            removeMenu.parentNode.removeChild(no);
-        }
-  }else{
-
+/*=======================================================*/
+function MenuUl(){
+   const addMenuUl = elMenu.appendChild(menuUl);
+         addMenuUl.classList.add("navbar-nav");
+    addLinks();
+       
   }
+
+/*====================================================================================================================== */
+
+function MenuH1(){
+  const addMenuUl = elMenuH1.appendChild(menuUl);
+           addMenuUl.classList.add("navbar-nav");
+  addLinks();
+
 }
-menuMediaQuery(consultaMedia);*/
+
+function moveMenu(){ ///ALTERA O MENU DE ACORDO COM O TAMANHO DA JANELA 
+    let mediaMenu = window.matchMedia('(min-width: 768px)');
+    
+    if(mediaMenu.matches){
+      MenuH1();
+     }else{
+      MenuUl();
+     }
+  }
+document.addEventListener('DOMContentLoaded',moveMenu);
+//document.addEventListener('change',moveMenu);
+
+
